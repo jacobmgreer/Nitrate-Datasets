1be13e7192bf68db3fe611739cdfc063fc9f0d9e
Jacob Greer <jacobmgreer@users.noreply.github.com>
2024-06-26 12:02:43 -0700

    adds

--- R/S3 - AltID datasets.R
+++ R/S3 - AltID datasets.R
@@ -1,49 +1,49 @@
-
-wd_filmfreeway <- 
-  jsonlite::fromJSON("json/wd_filmfreeway.json")$results$bindings %>%
-  reframe(
-    QID = basename(item$value),
-    filmfreeway = value$value)
+tryCatch({
+  wd_filmfreeway <- 
+    jsonlite::fromJSON("json/wd_filmfreeway.json")$results$bindings %>%
+    reframe(
+      QID = basename(item$value),
+      filmfreeway = value$value)
+    
+  write_csv(wd_filmfreeway, "datasets/event/wd_filmfreeway.csv")
   
-write_csv(wd_filmfreeway, "datasets/event/wd_filmfreeway.csv")
-
-wd_filmaffinity <- 
-  jsonlite::fromJSON("json/wd_filmaffinity.json")$results$bindings %>%
-  reframe(
-    QID = basename(item$value),
-    eidr = value$value)
-
-write_csv(wd_filmaffinity, "datasets/film/wd_filmaffinity.csv")
-
-wd_letterboxd <- 
-  jsonlite::fromJSON("json/wd_letterboxd.json")$results$bindings %>%
-  reframe(
-    QID = basename(item$value),
-    letterboxd = value$value)
-
-write_csv(wd_letterboxd, "datasets/film/wd_letterboxd.csv")
-
-wd_tcm <- 
-  jsonlite::fromJSON("json/wd_tcm.json")$results$bindings %>%
-  reframe(
-    QID = basename(item$value),
-    tcm = value$value)
-
-write_csv(wd_tcm, "datasets/film/wd_tcm.csv")
-
-wd_eidrparty <- 
-  jsonlite::fromJSON("json/wd_eidrparty.json")$results$bindings %>%
-  reframe(
-    QID = basename(item$value),
-    eidrparty = value$value)
-
-write_csv(wd_eidrparty, "datasets/companies/wd_eidrparty.csv")
-
-wd_eidr <- 
-  jsonlite::fromJSON("json/wd_eidr.json")$results$bindings %>%
-  reframe(
-    QID = basename(item$value),
-    eidr = value$value)
-
-write_csv(wd_eidr, "datasets/film/wd_eidr.csv")
-
+  wd_filmaffinity <- 
+    jsonlite::fromJSON("json/wd_filmaffinity.json")$results$bindings %>%
+    reframe(
+      QID = basename(item$value),
+      eidr = value$value)
+  
+  write_csv(wd_filmaffinity, "datasets/film/wd_filmaffinity.csv")
+  
+  wd_letterboxd <- 
+    jsonlite::fromJSON("json/wd_letterboxd.json")$results$bindings %>%
+    reframe(
+      QID = basename(item$value),
+      letterboxd = value$value)
+  
+  write_csv(wd_letterboxd, "datasets/film/wd_letterboxd.csv")
+  
+  wd_tcm <- 
+    jsonlite::fromJSON("json/wd_tcm.json")$results$bindings %>%
+    reframe(
+      QID = basename(item$value),
+      tcm = value$value)
+  
+  write_csv(wd_tcm, "datasets/film/wd_tcm.csv")
+  
+  wd_eidrparty <- 
+    jsonlite::fromJSON("json/wd_eidrparty.json")$results$bindings %>%
+    reframe(
+      QID = basename(item$value),
+      eidrparty = value$value)
+  
+  write_csv(wd_eidrparty, "datasets/companies/wd_eidrparty.csv")
+  
+  wd_eidr <- 
+    jsonlite::fromJSON("json/wd_eidr.json")$results$bindings %>%
+    reframe(
+      QID = basename(item$value),
+      eidr = value$value)
+  
+  write_csv(wd_eidr, "datasets/film/wd_eidr.csv")
+}, error = function(err){})
